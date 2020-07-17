8b5d81212597a42ac5026dcefbd691e2d22ad217
Erik <kimseyerik@gmail.com>
2020-07-17 11:02:49 -0400

    Ya

---
+++ Assets/Scripts/BezierCurve.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierCurve : MonoBehaviour
+{
+    public int SEG_COUNT;
+    public int CTRL_PT_COUNT;
+    private float t;
+    private Vector3[] ctrlPointsPositions;
+    private Vector3[] theCurve;
+
+    // public 
+    
+    void Start()
+    {
+        t = 1/SEG_COUNT;
+        theCurve = new Vector3[SEG_COUNT];
+        
+        CreatePoints();
+    }
+
+    private void CreatePoints(){
+        ctrlPointsPositions = new Vector3[]{
+            new Vector3(0,0,0),
+            new Vector3(1,2,2),
+            new Vector3(1,4,4),
+            new Vector3(4,2,0)
+        };
+    }
+
+    private void RenderCurve(){
+        // line = 
+        for (int i = 0; i < SEG_COUNT; i++)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Mesh_Generator.cs
+++ Assets/Scripts/Mesh_Generator.cs
@@ -34,77 +34,74 @@ public class Mesh_Generator : MonoBehaviour
     }
 
     void CalculateTotalVertices(){
-        // int e = ((xSize + ySize + zSize) * 4) + 3;
-        // int f = ((xSize+1)*(ySize+1) + (xSize+1)*(zSize+1) + (ySize+1)*(zSize+1)) * 2;
-        // TOTAL_VERTICES = CORNER_VERT_TOTAL + f + e;
-        TOTAL_VERTICES = 6;
+        int e = ((xSize + ySize + zSize) * 4) + 3;
+        int f = ((xSize+1)*(ySize+1) + (xSize+1)*(zSize+1) + (ySize+1)*(zSize+1)) * 2;
+        TOTAL_VERTICES = f + e;
         Debug.Log("TOTAL_VERTICES");
         Debug.Log(TOTAL_VERTICES);
-        vertices = new Vector3[6];
+        vertices = new Vector3[TOTAL_VERTICES];
         StartCoroutine(MakeMeshData());
     }
 
     private IEnumerator MakeMeshData(){
         WaitForSeconds wait = new WaitForSeconds(0.0f);
-        // for (int i = 0; i < TOTAL_VERTICES; i++)
+        // for (int i = 0; i < TOTAL_VERTICES; i+=6)
         // {
-        //     vertices[i] = 
+        //     vertices[0] = new Vector3(i,i,i);
+        //     vertices[i + 1] = new Vector3(i, i + 1, i);
+        //     vertices[i + 2] = new Vector3(i + 1,i,i);
+        //     vertices[i + 3] = new Vector3(i + 1,i,i);
+        //     vertices[i + 4] = new Vector3(i, i+1 ,i+1);
+        //     vertices[i + 5] = new Vector3(i+1 ,i+1 ,i);
         // }
-        vertices[0] = new Vector3(-1,0,0);
-        vertices[1] = new Vector3(0,4,5);
-        vertices[2] = new Vector3(4,0,0);
-        vertices[3] = new Vector3(4,0,0);
-        vertices[4] = new Vector3(0,4,2);
-        vertices[5] = new Vector3(4,4,0);
-        // for (int y = 0; y < ySize; y++)
-        // {
-        //     for (int x = 0; x <= xSize; x++) 
-        //     {
-        //         vertices[vertCount] = new Vector3(x, y, 0);
-        //         vertCount++;
-        //         yield return wait;
-        //     }
-        //     for (int z = 1; z <= zSize; z++)
-        //     {
-        //         vertices[vertCount] = new Vector3(0, y, z);
-        //         vertCount++;
-        //         yield return wait;
-        //     }
-        //     for (int x = xSize; x >= 0; x--)
-        //     {
-        //         vertices[vertCount] = new Vector3(x, y, zSize);
-        //         vertCount++;
-        //         yield return wait;
-        //     }
-        //     for (int z = zSize - 1 ; z > 0; z--)
-        //     {
-        //         vertices[vertCount] = new Vector3(xSize, y, z);
-        //         vertCount++;
-        //         yield return wait;
-        //     }
-        //     vertCount++;
-        // }
-        // for (int z = 1; z <= zSize; z++) {
-        //     for (int x = 1; x < xSize; x++) {
-        //         vertices[vertCount] = new Vector3(x, 0, z);
-        //         vertCount++;
-        //         yield return wait;
-        //     }
-        // }
-        // for (int z = 0; z < zSize + 1; z++) {
-        //     for (int x = 0; x <= xSize; x++) {
-        //         vertices[vertCount] = new Vector3(x, ySize, z);
-        //         vertCount++;
-        //         yield return wait;
-        //     }
-        // }
-        yield return wait;
+        for (int y = 0; y < ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++) 
+            {
+                vertices[vertCount] = new Vector3(x, y, 0);
+                vertCount++;
+                yield return wait;
+            }
+            for (int z = 1; z <= zSize; z++)
+            {
+                vertices[vertCount] = new Vector3(0, y, z);
+                vertCount++;
+                yield return wait;
+            }
+            for (int x = xSize; x >= 0; x--)
+            {
+                vertices[vertCount] = new Vector3(x, y, zSize);
+                vertCount++;
+                yield return wait;
+            }
+            for (int z = zSize - 1 ; z > 0; z--)
+            {
+                vertices[vertCount] = new Vector3(xSize, y, z);
+                vertCount++;
+                yield return wait;
+            }
+            vertCount++;
+        }
+        for (int z = 1; z <= zSize; z++) {
+            for (int x = 1; x < xSize; x++) {
+                vertices[vertCount] = new Vector3(x, 0, z);
+                vertCount++;
+                yield return wait;
+            }
+        }
+        for (int z = 0; z < zSize + 1; z++) {
+            for (int x = 0; x <= xSize; x++) {
+                vertices[vertCount] = new Vector3(x, ySize, z);
+                vertCount++;
+                yield return wait;
+            }
+        }
         CreateTriangles();
     }
 
 
     private void CreateTriangles () {
-        tris = new int[6];
+        tris = new int[vertices.Length];
         Debug.Log("tris.Length");
         Debug.Log(tris.Length);
         GenerateTris();
@@ -113,12 +110,7 @@ public class Mesh_Generator : MonoBehaviour
     // 0,1,2,2,1,3,3,2,4
 
     private void GenerateTris(){
-            tris[0] = 0;
-            tris[1] = 1;
-            tris[2] = 2;
-            tris[3] = 3;
-            tris[4] = 4;
-            tris[5] = 5;
+        
         GenerateMeshData();
     }
 
